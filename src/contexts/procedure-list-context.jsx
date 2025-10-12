56d67273bc6ce79ab65d0d2c5b78aae18128bac1
Dr. Ali Aafee <45202@igmh.gov.mv>
2025-10-12 07:53:17 +0500

    Moved reducer to a seperate file

--- src/contexts/procedure-list-context.jsx
+++ src/contexts/procedure-list-context.jsx
@@ -6,104 +6,13 @@ import {
     useState,
 } from "react";
 import { pb } from "@/lib/pb";
-
-const procedureListReducer = (state, action) => {
-    switch (action.type) {
-        case "SET_LIST":
-            if (state === null) {
-                return {
-                    procedures: action.payload,
-                    selected: null,
-                    updating: [],
-                    update_failed: [],
-                };
-            }
-            return {
-                ...state,
-                procedures: action.payload,
-            };
-        case "SET_SELECTED":
-            return {
-                ...state,
-                selected: action.payload,
-            };
-        case "ADD_PROCEDURE":
-            return {
-                ...state,
-                procedures: [...state.procedures, action.payload],
-            };
-        case "UPDATE_ID":
-            const procedure = state.procedures.find(
-                (p) => p.id === action.payload.id
-            );
-            return {
-                ...state,
-                procedures: [
-                    ...state.procedures.filter(
-                        (p) => p.id !== action.payload.id
-                    ),
-                    {
-                        ...procedure,
-                        id: action.payload.newId,
-                        patient: action.payload.newPatientId,
-                        expand: {
-                            ...procedure.expand,
-                            patient: {
-                                ...procedure.expand.patient,
-                                id: action.payload.newPatientId,
-                            },
-                        },
-                    },
-                ],
-                selected:
-                    state.selected === action.payload.id
-                        ? action.payload.newId
-                        : state.selected,
-            };
-        case "UPDATE_PROCEDURE":
-            const originalProcedure = state.procedures.find(
-                (p) => p.id === action.payload.id
-            );
-            return {
-                ...state,
-                procedures: [
-                    ...state.procedures.filter(
-                        (p) => p.id !== action.payload.id
-                    ),
-                    {
-                        ...originalProcedure,
-                        ...action.payload,
-                    },
-                ],
-            };
-
-        case "ADD_UPDATING":
-            return {
-                ...state,
-                updating: [...state.updating, ...action.payload],
-            };
-        case "DONE_UPDATING":
-            return {
-                ...state,
-                updating: state.updating.filter(
-                    (item) => !!!action.payload.includes(item)
-                ),
-            };
-        case "ADD_FAILED":
-            return {
-                ...state,
-                update_failed: [...state.update_failed, ...action.payload],
-            };
-        default:
-            return state;
-    }
-};
+import ProcedureListReducer from "@/reducers/procedure-list-reducer";
 
 const ProcedureListContext = createContext(null);
 
 export function ProcedureListProvider({ children }) {
     const [proceduresList, dispatchData] = useReducer(
-        procedureListReducer,
+        ProcedureListReducer,
         null
     );
     const [otDay, setOtDay] = useState(null);
---
+++ src/reducers/procedure-list-reducer.jsx
@@ -0,0 +1,99 @@
+function ProcedureListReducer(state, action) {
+    switch (action.type) {
+        case "SET_LIST":
+            if (state === null) {
+                return {
+                    procedures: action.payload,
+                    selected: null,
+                    updating: [],
+                    update_failed: [],
+                };
+            }
+            return {
+                ...state,
+                procedures: action.payload,
+            };
+        case "SET_SELECTED":
+            return {
+                ...state,
+                selected: action.payload,
+            };
+        case "ADD_PROCEDURE":
+            return {
+                ...state,
+                procedures: [...state.procedures, action.payload],
+            };
+        case "UPDATE_ID":
+            const procedure = state.procedures.find(
+                (p) => p.id === action.payload.id
+            );
+            if (procedure === undefined) {
+                return state;
+            }
+            return {
+                ...state,
+                procedures: [
+                    ...state.procedures.filter(
+                        (p) => p.id !== action.payload.id
+                    ),
+                    {
+                        ...procedure,
+                        id: action.payload.newId,
+                        patient: action.payload.newPatientId,
+                        expand: {
+                            ...procedure.expand,
+                            patient: {
+                                ...procedure.expand.patient,
+                                id: action.payload.newPatientId,
+                            },
+                        },
+                    },
+                ],
+                selected:
+                    state.selected === action.payload.id
+                        ? action.payload.newId
+                        : state.selected,
+            };
+        case "UPDATE_PROCEDURE":
+            const originalProcedure = state.procedures.find(
+                (p) => p.id === action.payload.id
+            );
+            if (originalProcedure === undefined) {
+                return state;
+            }
+            return {
+                ...state,
+                procedures: [
+                    ...state.procedures.filter(
+                        (p) => p.id !== action.payload.id
+                    ),
+                    {
+                        ...originalProcedure,
+                        ...action.payload,
+                    },
+                ],
+            };
+
+        case "ADD_UPDATING":
+            return {
+                ...state,
+                updating: [...state.updating, ...action.payload],
+            };
+        case "DONE_UPDATING":
+            return {
+                ...state,
+                updating: state.updating.filter(
+                    (item) => !!!action.payload.includes(item)
+                ),
+            };
+        case "ADD_FAILED":
+            return {
+                ...state,
+                update_failed: [...state.update_failed, ...action.payload],
+            };
+        default:
+            return state;
+    }
+}
+
+export default ProcedureListReducer;
