5869ea0c6cc4a432d037bfd80fdde6d49bd68a66
Dr. Ali Aafee <45202@igmh.gov.mv>
2025-12-21 13:10:45 +0500

    Fixed user permissions

---
+++ pb/pb_migrations/1766304506_updated_users.js
@@ -0,0 +1,22 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "updateRule": "(id = @request.auth.id && (@request.body.role:isset = false)) || (@request.auth.id != \"\" && @request.auth.role = \"admin\")",
+    "viewRule": "id = @request.auth.id || (@request.auth.id != \"\")"
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "updateRule": "id = @request.auth.id && (@request.body.role:isset = false)",
+    "viewRule": "id = @request.auth.id || (@request.auth.id != \"\" && @request.auth.role = \"admin\")"
+  }, collection)
+
+  return app.save(collection)
+})
